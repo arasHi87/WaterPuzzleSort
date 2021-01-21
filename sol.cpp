033163b28291ca7d19bd8f457b3049c6f46dbde4
arashi87 <arasi27676271@gmail.com>
2021-01-22 02:42:01 +0800

    first commit

---
+++ sol.cpp
@@ -0,0 +1,127 @@
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int maxN=1e3+10;
+typedef pair<int, int> pii;
+
+struct tube {
+    int len;
+    string block[5]; 
+    
+    tube(): len(0) { for (int i=0;i<4;i++) block[i]=""; };
+
+    int last() { return 4-len; }
+    
+    string pop() {  return block[len--]; }
+
+    string top() { return block[len]; }
+
+    void push(string x) { block[++len]=x; }
+    
+    void print() {
+        for (int i=1;i<=len;i++)
+            cout << block[i] << ' ';
+        puts("");
+    }
+
+    bool empty() { return !len; }
+
+    bool finish() {
+        if (!len) return true;
+        if (len && len!=4) return false;
+        for (int i=1;i<=3;i++)
+            if (block[i]!=block[i+1])
+                return false;
+        return true;
+    }
+
+    bool operator < (const tube &rhs) const {
+        if (len!=rhs.len) return len<rhs.len;
+        for (int i=1;i<=len;i++) {
+            if (block[i]!=rhs.block[i]) {
+                if (block[i].size()==rhs.block[i].size()) return block[i]<rhs.block[i];
+                else return block[i].size()<rhs.block[i].size();
+            }
+        }
+        return true;
+    }
+};
+
+int n, m, idx=1;
+tube tubes[maxN];
+set<string> vis;
+stack<pii> ans;
+
+bool check(tube tmp[]) {
+    for (int i=1;i<=idx;i++) {
+        if (!tmp[i].finish())
+            return false;
+    }
+    return true;
+}
+
+string hash_tube(tube tmp[]) {
+    tube arr[maxN];
+    string str;
+
+    for (int i=1;i<=idx;i++)
+        arr[i]=tmp[i];
+    
+    sort(arr, arr+idx+1);
+ 
+    for (int i=1;i<=idx;i++)
+        for (int j=1;j<=arr[i].len;j++)
+            str+=arr[i].block[j];
+    
+    return str;    
+}
+
+bool dfs(tube tmp[]) {
+    string _hash=hash_tube(tmp);
+    
+    if (vis.find(_hash)!=vis.end()) return false;
+    if (check(tmp)) return true;
+
+    vis.insert(_hash);
+
+    for (int i=1;i<=idx;i++) {
+        if (!tmp[i].empty()) {
+            for (int j=1;j<=idx;j++) {
+                if (i!=j) {
+                    int cnt=0;
+
+                    if ((tmp[j].empty()) || (tmp[j].last() && tmp[i].top()==tmp[j].top())) {
+                        while (!tmp[i].empty() && (tmp[j].empty() || (tmp[i].top()==tmp[j].top() && tmp[j].last())))
+                            cnt++, tmp[j].push(tmp[i].pop());
+                        
+                        if (dfs(tmp)) {
+                            ans.push(make_pair(i, j));
+                            return true;
+                        }
+                        
+                        while (cnt--) tmp[i].push(tmp[j].pop());
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    while (cin >> n >> m) {
+        if (tubes[idx].len==4) idx++;
+        while (m--)
+            tubes[idx].push(to_string(n));
+    } idx+=2, dfs(tubes);
+    
+    while (!ans.empty()) {
+        cout << ans.top().first << ' ' << ans.top().second << '\n';
+        ans.pop();
+    }
+}
---
+++ utils.py
@@ -0,0 +1,77 @@
+import cv2
+import random
+import numpy as np
+from pprint import pprint
+
+def label_color(src):
+    img = cv2.imread(src)
+    kernel = np.ones((5,5), np.uint8)
+    
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    open_img = cv2.morphologyEx(gray_img, cv2.MORPH_OPEN, kernel, iterations=3)
+    erosion_img = cv2.erode(open_img, np.ones((5, 5), np.uint8), iterations=1)
+    
+    ret, binary = cv2.threshold(erosion_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE) 
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
+    temp = list()
+    
+    max_width = max(bounding_boxes, key=lambda r: r[0] + r[2])[0]
+    max_height = max(bounding_boxes, key=lambda r: r[3])[3]
+    nearest = max_height * 1.4
+    bounding_boxes.sort(key=lambda r: (int(nearest * round(float(r[1])/nearest)) * max_width + r[0]))
+
+    for bbox in bounding_boxes:
+        [x, y, w, h] = bbox
+        stk = list()
+        
+        mid_x = int(x+w/2)
+        qut_h = int(h/4)
+        y += int(qut_h/2)
+
+        for i in range(3, -1, -1):
+            b, g, r = img[y+qut_h*i, mid_x]
+            stk.append([(r, g, b), 1])
+       
+        temp.append(stk)
+
+    return temp
+
+def merge_color(tubes):
+    for stk in tubes:
+        if stk:
+            for idx in range(0, 4):
+                if stk[idx] == stk[idx-1]:
+                    stk[idx-1][1]+=1
+                    del stk[idx]
+
+    return tubes
+
+def format_color(tubes):
+    idx = 1
+    cnvt = dict()
+    cont = list()
+
+    for stk in tubes:
+        for ele in stk:
+            if ele[0] not in cnvt:
+                cnvt[ele[0]] = idx
+                idx+=1
+            ele[0]=cnvt[ele[0]]
+
+    #  for key, val in cnvt.items():
+    #      print("{}, {}".format(key, val))
+
+    return tubes
+
+if __name__ == "__main__":
+    tubes = label_color('img.jpg')
+    tubes = merge_color(tubes)
+    tubes = format_color(tubes)
+
+    #  pprint(tubes)
+
+    for tube in tubes:
+        for block in tube:
+            print("{} {}".format(block[0], block[1]))
